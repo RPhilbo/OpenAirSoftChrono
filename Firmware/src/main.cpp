045fa93ec2f8da00f964eedbab7f7e6ee1aab483
DESKTOP-Z690\Phil <philipp.rathmann87@gmail.com>
2026-02-14 10:04:48 +0100

    Define and setup the mcu pins for the ToF sensor pcb.

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -7,6 +7,21 @@
 
 
 /* ============================================================
+ * ======================= Pins ===============================
+ * ============================================================ */
+#define LED_OFF HIGH
+#define LED_ON LOW
+
+#define ToF_Sensor1_Enable D9
+#define ToF_Sensor2_Enable D8
+#define ToF_Sensor3_Enable D19
+
+#define ToF_Sensor1_Output D10
+#define ToF_Sensor2_Output D7
+#define ToF_Sensor3_Output D18
+
+
+/* ============================================================
  * ======================= RTOS ===============================
  * ============================================================ */
 // Task handlers to use multitasking
@@ -37,6 +52,16 @@ void setup() {
   pinMode(LED_RED, OUTPUT);   digitalWrite(LED_RED,   LED_OFF);
   pinMode(LED_GREEN, OUTPUT); digitalWrite(LED_GREEN, LED_OFF);
   pinMode(LED_BLUE, OUTPUT);  digitalWrite(LED_BLUE,  LED_OFF);
+
+  // Setup the ToF sensor pins
+  pinMode(ToF_Sensor1_Enable, OUTPUT);   digitalWrite(ToF_Sensor1_Enable, LOW);
+  pinMode(ToF_Sensor2_Enable, OUTPUT);   digitalWrite(ToF_Sensor2_Enable, LOW);
+  pinMode(ToF_Sensor3_Enable, OUTPUT);   digitalWrite(ToF_Sensor3_Enable, LOW);
+
+  pinMode(ToF_Sensor1_Output, INPUT_PULLDOWN);
+  pinMode(ToF_Sensor2_Output, INPUT_PULLDOWN);
+  pinMode(ToF_Sensor3_Output, INPUT_PULLDOWN);
+
 
   startTasksSignal = xSemaphoreCreateBinary(); // Create the "Pistol"
 
